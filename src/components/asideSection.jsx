7ef2b07f9479ecd152600bd085d1e85629888427
Olayinka-2 <olayinkaisrael802@gmail.com>
2024-12-31 04:47:37 +0100

    fix: clearing the info on a change

--- src/components/asideSection.jsx
+++ src/components/asideSection.jsx
@@ -1,61 +1,53 @@
-export default function Aside({profileInfo}) {
-   let placeholderEmail = "";
-   let placeholderTel = "";
-   let placeholderGit = "";
-   let placeholderLinkedIn = "";
+   const defaultContactInfo = {
+   telephone: '09000550605',
+   github: 'https://github.com/olayinka-2',
+   linkedIn: 'linkedin.com/in/israel.ola',
+   email: 'Johndoe@gmail.com',
+   };
 
-   if(profileInfo["email"] == "") {
-      placeholderEmail = 'Johndoe@gmail.com';
-   } else {
-      placeholderEmail = profileInfo["email"]
+   function Aside({ profileInfo }) {
+      let contactDetails = {};
+
+   // Combine default values with profile info using spread syntax
+   for(let element in profileInfo) {
+      if(profileInfo[element] !== "")  {
+         contactDetails[element] = profileInfo[element];
+      } else {
+         contactDetails[element] = defaultContactInfo[element]
+      }
    }
-
-   if(profileInfo["telephone"] == "") {
-      placeholderTel = '09000550605';
-   } else {
-      placeholderTel = profileInfo["telephone"]
-   }
-
-   if(profileInfo["github"] == "") {
-      placeholderGit = "https://github.com/olayinka-2";
-   } else {
-      placeholderGit = profileInfo["github"]
-   }
-
-   if(profileInfo["linkedIn"] == "") {
-      placeholderLinkedIn = "linkedin.com/in/israel.ola";
-   } else {
-      placeholderLinkedIn = profileInfo["linkedIn"]
-   }
-
+   
    return (
-      <>
       <aside>
-                  <div className="profile-picture">
-                        <img src="profile-photo.jpg" alt="profile-photo"/>
-                  </div>
-                  <div className="contact-container">
-                     <div className="contact-heading">
-                        Contact
-                     </div>
-                     <div>
-                        <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/phone-disconnected.png" alt="phone-disconnected"/>
-                        <p>{placeholderTel}</p>
-                     </div>
-                     <div>
-                        <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/github.png" alt="github"/>
-                        <p>{placeholderGit}</p>
-                     </div>
-                     <div>
-                        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin"/>
-                        <p>{placeholderLinkedIn}</p>
-                     </div>
-                     <div>
-                        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/new-post.png" alt="new-post"/>
-                        <p>{placeholderEmail}</p>
-                     </div>
-                  </div>
-               </aside>
-      </>
-   )
-}
+         <div className="profile-picture">
+         <img src="profile-photo.jpg" alt="profile-photo" />
+         </div>
+         <div className="contact-container">
+         <div className="contact-heading">Contact</div>
+         <div>
+            <img
+               width="30"
+               height="30"
+               src="https://img.icons8.com/ios-glyphs/30/phone-disconnected.png"
+               alt="phone-disconnected"
+            />
+            <p>{contactDetails.telephone}</p>
+         </div>
+         <div>
+            <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/github.png" alt="github" />
+            <p>{contactDetails.github}</p>
+         </div>
+         <div>
+            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/linkedin.png" alt="linkedin" />
+            <p>{contactDetails.linkedIn}</p>
+         </div>
+         <div>
+            <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/new-post.png" alt="new-post" />
+            <p>{contactDetails.email}</p>
+         </div>
+         </div>
+      </aside>
+   );
+   }
+
+   export default Aside;
